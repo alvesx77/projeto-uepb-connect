e9ced5e0deea25add7431469b878003a9a29341a
DevItaloDias <italodds02@gmail.com>
2026-07-18 01:28:09 -0300

    refactor: removendo todo o backend no frontend

--- backendLab/src/main/java/com/backendProjeto/backendLab/Erros/DadoDuplicadoException.java
+++
@@ -1,7 +0,0 @@
-package com.backendProjeto.backendLab.Erros;
-
-public class DadoDuplicadoException extends RuntimeException{
-    public DadoDuplicadoException(String mensagemError){
-        super(mensagemError);
-    }
-}
